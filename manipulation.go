9cf475a5702d11ddfd99812dd58c1bcb3894db09
sam <aquaticcalf@proton.me>
2024-12-29 12:56:04 +0530

    meow

--- manipulation.go
+++ manipulation.go
@@ -1,5 +1,10 @@
 package stringsattached
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Reverse(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes) - 1; i < j; i, j = i + 1, j -1 {
@@ -8,3 +13,13 @@ func Reverse(s string) string {
 	return string(runes)
 } // returns the reverse of a string ( abcdefgh -> hgfedcba )
 
+func Is_palindrome(s string) bool {
+	var cleaned strings.Builder
+	for _, r := range strings.ToLower(s) {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			cleaned.WriteRune(r)
+		}
+	}
+	clean_string := cleaned.String()
+	return clean_string == Reverse(clean_string)
+}
--- tests/test_manipulation.go
+++ tests/test_manipulation.go
@@ -6,14 +6,24 @@ import (
 )
 
 func test_reverse() {
+	fmt.Println("-------------")
 	fmt.Println("testing Reverse...")
 	fmt.Println("\"hello\" -> \"olleh\" :",stringsattached.Reverse("hello") == "olleh")
 	fmt.Println("\"\" -> \"\" :",stringsattached.Reverse("") == "")
 	fmt.Println("\"racecar\" -> \"racecar\" :",stringsattached.Reverse("racecar") == "racecar")
 }
 
+func test_is_palindrome() {
+	fmt.Println("-------------")
+	fmt.Println("testing Is_palindrome...")
+	fmt.Println("\"A man, a plan, a canal, Panama\" -> palindrome :",stringsattached.Is_palindrome("A man, a plan, a canal, Panama") == true)
+	fmt.Println("\"hello\" -> not a palindrome :",stringsattached.Is_palindrome("hello") == false)
+	fmt.Println("\"\" -> palindrome :",stringsattached.Is_palindrome("") == true)
+
+}
+
 func main() {
 	fmt.Println("running tests")
-	fmt.Println("-------------")
 	test_reverse()
+	test_is_palindrome()
 }
